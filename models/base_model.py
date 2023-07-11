f7ad446c45def2bf3a07ca7cdb9f4eb52ab35346
mosesgitonga <infosec947@gmail.com>
2023-07-10 22:37:57 -0700

    added docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
+""" the base model class"""
+
+
 import uuid
 from datetime import datetime
+""" nd"""
+
 
 class BaseModel:
+    """
+    Base class for other models.
+    """
     def __init__(self, my_number=None, name=None):
+        """
+        Initializes a new instance of the BaseModel.
+
+        Args:
+            my_number (int): The number associated with the instance.
+            name (str): The name associated with the instance.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -11,22 +26,40 @@ class BaseModel:
         self.name = name
 
     def __str__(self):
-        return "[{}] ({}) {{'my_number': {}, 'name': {}, 'updated_at': {}, 'id': '{}', 'created_at': {}}}".format(
-                self.__class__.__name__,
-                self.id,
-                self.my_number,
-                self.name,
-                self.updated_at,
-                self.id,
-                self.created_at
-                )
+        """
+        Returns a string representation of the BaseModel.
+
+        Returns:
+            str: The string representation of the BaseModel.
+        """
+        return "[{}] ({}) {{'my_number': {}, 'name': {}, 'updat\
+ed_at': {}, 'id': '{}', 'created_at': {}}}".format(
+            self.__class__.__name__,
+            self.id,
+            self.my_number,
+            self.name,
+            self.updated_at.strftime("%Y, %m, %d, %H, %M, %S, %f"),
+            self.id,
+            self.created_at.strftime("%Y, %m, %d, %H, %M, %S, %f")
+        )
 
     def save(self):
+        """
+        Updates the `updated_at` attribute with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel.
+
+        Returns:
+            dict: A dictionary containing all\
+                    keys/values of the BaseModel instance.
+        """
         data = {
             '__class__': self.__class__.__name__,
+            'id': self.id,
             'created_at': self.created_at.isoformat(),
             'updated_at': self.updated_at.isoformat()
         }
@@ -35,4 +68,3 @@ class BaseModel:
         if self.name is not None:
             data['name'] = self.name
         return data
-
