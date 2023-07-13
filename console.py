e19dabb4a7c4ee143a01108b6256ef211a4b8da2
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-07-13 11:16:34 +0000

    Task 7 Updates

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """import commands"""
 
+from models import storage
+from models.base_model import BaseModel
+import json
+import re
 import cmd
 """ console"""
 
@@ -38,6 +42,72 @@ class HBNBCommand(cmd.Cmd):
             print("EOF  help  quit")
             print()
 
+    def do_create(self, arg):
+        """
+            The Method Creates a new instance of BaseModel
+            and saves it to a json file and prints its id
+        """
+        if arg == "" or line is None:
+            print("** class name missing **")
+
+        elif arg not in storage.classes():
+            print("** class doesn't exist **")
+
+        else:
+            my_arg = storage.classes()[arg]()
+            my_arg.save()
+            print(my_arg.id)     
+
+    def do_show(self, myline):
+        """
+            This Method Prints the String Representation
+            of an Instance based on the class name and id
+        """
+        #Check if myline is empty or none
+        if myline == "" or myline is None:
+            print("** class name missing **")
+        #If myline is empty then ...
+        else:
+            #Split myline into words using the space char
+            mywords = myline.split(' ')
+            #Check if first word is present in storage
+            if mywords[0] not in storage.classes():
+                print("** class doesn't exist **")
+            #Check the Second word which is the id
+            elif len(mywords) < 2:
+                print("** instance id missing **")
+            #If both exist then ...
+            else: #Concatenate both strs with a dot between
+                key = "{}.{}".format(mywords[0], mywords[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else: #If key is found
+                    #print the string representation
+                    print(storage.all()[key])
+
+    def do_destroy(self, my_line):
+        """
+            This Method performs the task of deleting an
+            Instance.
+            Parameters:
+                my_line - A String representing the
+                    command to be executed
+        """
+        if my_line == "" or my_line is None:
+            print("** class name missing **")
+        else:
+            mywords = my_line.split(' ')
+            if mywords[0] not in storage.classes():
+                print("** class doesn't exist")
+            elif len(mywords) < 2:
+                print("** instance id missing **")
+            else:
+                mykey = "{}.{}".format(mywords[0], mywords[1])
+                if mykey not in storage.all():
+                    print("** no instance found **")
+                else: #Delete the instance
+                    del storage.all()[mykey]
+                    storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
