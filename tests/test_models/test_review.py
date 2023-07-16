599d517267c582ff49dbc3ad34fb789f8736a128
mosesgitonga <infosec947@gmail.com>
2023-07-16 10:30:00 -0700

    review.py

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.review import Review
+
+class TestReview_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Review class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(Review, type(Review()))
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        rv = Review()
+        rv.id = "123456"
+        rv.created_at = rv.updated_at = dt
+        rvstr = rv.__str__()
+        self.assertIn("[Review] (123456)", rvstr)
+        self.assertIn("'id': '123456'", rvstr)
+        self.assertIn("'created_at': " + dt_repr, rvstr)
+        self.assertIn("'updated_at': " + dt_repr, rvstr)
+
+    def test_args_unused(self):
+        rv = Review(None)
+        self.assertNotIn(None, rv.__dict__.values())
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        rv = Review(id="345", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(rv.id, "345")
+        self.assertEqual(rv.created_at, dt)
+        self.assertEqual(rv.updated_at, dt)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            Review(id=None, created_at=None, updated_at=None)
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(Review(), models.storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(Review().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Review().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(Review().updated_at))
+
+    def test_place_id_is_public_class_attribute(self):
+        rv = Review()
+        self.assertEqual(str, type(Review.place_id))
+        self.assertIn("place_id", dir(rv))
+        self.assertNotIn("place_id", rv.__dict__)
+
+    def test_user_id_is_public_class_attribute(self):
+        rv = Review()
+        self.assertEqual(str, type(Review.user_id))
+        self.assertIn("user_id", dir(rv))
+        self.assertNotIn("user_id", rv.__dict__)
+
+    def test_text_is_public_class_attribute(self):
+        rv = Review()
+        self.assertEqual(str, type(Review.text))
+        self.assertIn("text", dir(rv))
+        self.assertNotIn("text", rv.__dict__)
+
+    def test_two_reviews_unique_ids(self):
+        rv1 = Review()
+        rv2 = Review()
+        self.assertNotEqual(rv1.id, rv2.id)
+
+    def test_two_reviews_different_created_at(self):
+        rv1 = Review()
+        sleep(0.05)
+        rv2 = Review()
+        self.assertLess(rv1.created_at, rv2.created_at)
+
+    def test_two_reviews_different_updated_at(self):
+        rv1 = Review()
+        sleep(0.05)
+        rv2 = Review()
+        self.assertLess(rv1.updated_at, rv2.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
